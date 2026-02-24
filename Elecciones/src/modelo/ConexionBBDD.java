053652c118068f859e23e42053c139215af32ca3
Santiago901S <letradosantiago9@gmail.com>
2026-02-24 18:43:50 +0100

    Vale en este commit lo que hicimos fue : Añadir librerias, corregir la conexion y meter la informacion de la base de datos para conectarla

--- Elecciones/src/modelo/ConexionBBDD.java
+++ Elecciones/src/modelo/ConexionBBDD.java
@@ -12,43 +12,37 @@ public class ConexionBBDD {
 
     static {
         try {
-            // Cargamos el archivo de configuración
-            props.load(new FileInputStream("db.properties"));
+            // Ajusta la ruta si tu archivo está en una carpeta distinta
+            props.load(new FileInputStream("resources/database.properties"));
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("No se pudo cargar el archivo de propiedades");
         }
-    }
-
-    public static Connection getConexion() throws SQLException {
-        return DriverManager.getConnection(
-            props.getProperty("db.url"),
-            props.getProperty("db.user"),
-            props.getProperty("db.password")
-        );
     }
 
     public static ArrayList<Comunidad> obtenerComunidades() {
         ArrayList<Comunidad> lista = new ArrayList<>();
-        String sql = "SELECT * FROM PORCENTAJES_RANGOEDAD"; // Según tu SQL [cite: 10, 11]
+        // Query para la tabla de tu SQL
+        String sql = "SELECT * FROM PORCENTAJES_RANGOEDAD";
 
-        try (Connection con = getConexion();
+        try (Connection con = DriverManager.getConnection(
+                props.getProperty("database.url"),
+                props.getProperty("database.user"),
+                props.getProperty("database.password"));
              Statement st = con.createStatement();
              ResultSet rs = st.executeQuery(sql)) {
 
             while (rs.next()) {
-                // Creamos el objeto con los datos del censo [cite: 15]
-                Comunidad c = new Comunidad(
+                lista.add(new Comunidad(
                     rs.getString("NOMBRE_COMUNIDAD"),
                     rs.getInt("TOTAL_HABITANTES"),
                     rs.getInt("RANGO_18_25"),
                     rs.getInt("RANGO_26_40"),
                     rs.getInt("RANGO_41_65"),
                     rs.getInt("RANGO_MAS_66")
-                );
-                lista.add(c);
+                ));
             }
         } catch (SQLException e) {
-            System.err.println("Error en Acceso a Datos: " + e.getMessage());
+            e.printStackTrace();
         }
         return lista;
     }
